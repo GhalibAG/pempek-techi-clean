2c7b809405b8736b48ab9b1854c794b51ba471f2
GhalibAG <ghalibgtx@gmail.com>
2025-12-07 06:28:10 +0700

    Fix Http Issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider; // <--- Tambahan penting
 use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
 use Symfony\Component\Mailer\Transport\Dsn;
 
@@ -22,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Paksa HTTPS jika di Production (Railway)
+        if ($this->app->environment('production') || env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
+        // Settingan Email Brevo
         Mail::extend('brevo', function () {
             return (new BrevoTransportFactory)->create(
                 new Dsn(
